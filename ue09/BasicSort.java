7f075f5142a064f9e24ae3426af562cd0f98635b
Raiden Erdmann <raiden.erdmann@gmail.com>
2021-06-22 12:57:31 +0200

    Add insertion, bubble and quick sort

--- ue09/BasicSort.java
+++ ue09/BasicSort.java
@@ -1,6 +1,7 @@
 package ue09;
 
 public class BasicSort {
+
     // Selectionsort:
     // Sortiere das Teilfeld von array beginnend mit Index links bis einschließlich
     // Index rechts
@@ -24,13 +25,30 @@ public class BasicSort {
     // Sortiere das Teilfeld von array beginnend mit Index links bis einschließlich
     // Index rechts
     public static void insertionsort(int[] array, final int links, final int rechts) {
-        // TODO: Praktikum 9
+        for (int i = links + 1; i < rechts + 1; i++) {
+            // System.out.print("for...");
+            int x = array[i];
+            int j = i - 1;
+            while (j >= links && array[j] > x) {
+                array[j + 1] = array[j];
+                j--;
+            }
+            array[j + 1] = x;
+        }
     }
 
     // Bubblesort:
     // Sortiere das Teilfeld von array beginnend mit Index links bis einschließlich
     // Index rechts
     public static void bubblesort(int[] array, final int links, final int rechts) {
-        // TODO: Praktikum 9
+        for (int i = links; i < rechts + 1; i++) {
+            for (int j = links; j < rechts - i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int t = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = t;
+                }
+            }
+        }
     }
 }
--- ue09/QuickSort.java
+++ ue09/QuickSort.java
@@ -2,6 +2,25 @@ package ue09;
 
 public class QuickSort {
     public static void quicksort(int[] array, final int links, final int rechts) {
-        // TODO: Praktikum 9
+        int li = links;
+        int re = rechts;
+        int mid = array[(links + rechts) / 2];
+        do {
+            while (array[li] < mid)
+                li++;
+            while (array[re] > mid)
+                re--;
+            if (li <= re) {
+                int t = array[re];
+                array[re] = array[li];
+                array[li] = t;
+                li++;
+                re--;
+            }
+        } while (li <= re);
+        if (links < re)
+            quicksort(array, links, re);
+        if (rechts > li)
+            quicksort(array, li, rechts);
     }
 }
